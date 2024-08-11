c0a853bc25c7d65e6379a8de74b0d5f49ea72914
Quaaludees <quaalud@gmail.com>
2024-08-12 01:40:53 +0300

    fix rev

--- src/components/FilmCard/FilmCard.jsx
+++ src/components/FilmCard/FilmCard.jsx
@@ -1,25 +1,29 @@
 import React from 'react';
-import styles from  './FilmCard.module.css';
+import styles from './FilmCard.module.css';
 import {Button} from '../Button';
 import StarIcon from '../icons/StarIcon.jsx';
 import LikeIcon from '../icons/LikeIcon.jsx';
 import FavIcon from '../icons/FavIcon.jsx';
 import cn from 'classnames';
+import {Maybe} from '../Maybe/index.js';
 
 
+const DEFAULT_FILM_NAME = 'Название не указано';
 const FilmCard = ({count, poster, filmName, isFavorites}) => {
     return <div className={styles.wrapper}>
-        <div className={styles.rating}>
-            <StarIcon/>
-            <p className={styles.count}>{count}</p>
-        </div>
+        <Maybe when={!!count}>
+            <div className={styles.rating}>
+                <StarIcon/>
+                <p className={styles.count}>{count}</p>
+            </div>
+        </Maybe>
         <div className={styles.poster}>
             <img src={poster} alt="poster"/>
         </div>
-        <div className={styles.title}><p>{filmName}</p></div>
+        <div className={styles.title}><p>{filmName || DEFAULT_FILM_NAME}</p></div>
         <div className={styles.favAction}>
             <Button variants={'text'} className={cn({
-                [styles.favorites] : isFavorites
+                [styles.favorites]: isFavorites
             })}>
                 {isFavorites ? <>
                     <FavIcon/>
--- src/components/SearchFilms/SearchFilms.jsx
+++ src/components/SearchFilms/SearchFilms.jsx
@@ -1,37 +1,24 @@
-import {useState} from 'react';
-import {Title} from '../Title/index.js';
-import {Paragraph} from '../Paragraph/index.js';
-import Input from '../Input/Input.jsx';
-import {Button} from '../Button/index.js';
+
 import styles from './SearchFilms.module.css';
 import FilmCard from '../FilmCard/FilmCard.jsx';
-import {FILMS_DATA, TEXT_CONTENT} from './constans';
+import {FILMS_DATA} from './constans';
+import SearchForm from '../SearchForm/SearchForm.jsx';
 
 
 
 const SearchFilms = () => {
-    const handleClick = () => {
-        console.log(inputData);
+    const handleClick = (value) => {
+        
+        console.log(value);
     };
-    const [inputData, setInputData] = useState('');
 
-
-    const handleChange = (event) => {
-        setInputData(event.target.value);
-    };
 
     return <>
-        <div className={styles.wrapper}>
-            <Title title="ПОИСК"/>
-            <Paragraph>{TEXT_CONTENT}</Paragraph>
-            <Input placeholder="Введите название" onChange={handleChange} isSearch/>
-            <Button onClick={handleClick}>Искать</Button>
-        </div>
+        <SearchForm onSubmit={handleClick}/>
         <div className={styles.list}>
-            {FILMS_DATA.map(({filmName,poster,count, isFavorites}) => (
-                <FilmCard key={filmName} filmName={filmName} poster={poster} isFavorites={isFavorites} count={count}/>
+            {FILMS_DATA?.map(({filmName,poster,count, isFavorites}, index) => (
+                <FilmCard key={`${filmName}_${index}`} filmName={filmName} poster={poster} isFavorites={isFavorites} count={count}/>
             ))}
-            {/*добавить проверку на пустой массив когда будет страницы !ой не найдено!*/}
         </div>
     </>;
 
